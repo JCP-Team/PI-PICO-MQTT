d239b26aaed26653609e83e1606d8f85ba6614bf
GitM3 <u04603852@tuks.co.za>
2022-09-18 15:52:41 +0200

    Added voltage monitor code, with error function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include <Arduino.h>
 #include "gsm_mqtt.h"
 
+#define ADC_PIN 26
+
+
 gsm_mqtt *gsm_module;
+float_t batt_v;
+float_t err;
 
 void mqtt_callback(String topic, String message){
     Serial.println(topic);
@@ -11,11 +16,19 @@ void mqtt_callback(String topic, String message){
 void setup() {
     Serial.begin(115200);
     while(!Serial);
-    gsm_module = new gsm_mqtt("test.mosquitto.org","commands",mqtt_callback);
+    pinMode(ADC_PIN,INPUT);
+    analogReadResolution(12);
+   
+     gsm_module = new gsm_mqtt("test.mosquitto.org","commands",mqtt_callback);
 }
 
 unsigned long int timerr = 0;
 void loop() {
+    batt_v = (analogRead(ADC_PIN) * (3.31/4095))*((560.0+156.0)/560.0) ;
+    err = (1/12) * batt_v + 0.065; //error function, determined.
+    batt_v += err;
+    Serial.print("Batt(V)"); Serial.println(batt_v,3);
+
     gsm_module->gsm_mqtt_loop();
     if(gsm_module->timeout(timerr)){
         timerr = gsm_module->set_time(60000*5);
@@ -23,5 +36,11 @@ void loop() {
     }else{
         return;
     }
+  
+  
+
+  
+
+    delay(1000);
 } 
 
